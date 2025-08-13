b9e62395ae176e08ec8b801618c6c713f8c3c4b7
gunnarguy <gunnarguy@me.com>
2025-08-13 16:26:10 -0700

    Initial Commit

---
+++ WoWCA/WoWCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoWCAApp.swift
+//  WoWCA
+//
+//  Created by Gunnar Hostetler on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WoWCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
